7794de5a01b93a00a826b6af1c92e70a36e6a63b
rajendra <rajendrammarboyana@gmail.com>
2024-04-05 08:15:14 +0100

    attaching role

--- module/main.tf
+++ module/main.tf
@@ -7,12 +7,33 @@ resource "aws_instance" "instance" {
   }
 }
 
-
-
 resource "aws_route53_record" "record" {
   name    = var.tool_name
   type    = "A"
   zone_id = var.zone_id
   records = [aws_instance.instance.public_ip]
   ttl = 30
+}
+resource "aws_iam_role" "role" {
+  name = "${var.tool_name}-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "${var.tool_name}-role"
+  }
 }
