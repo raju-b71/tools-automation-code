c42b4d6d032a6f4988d5a7c537ee1b2c884a920a
rajendra <rajendrammarboyana@gmail.com>
2024-04-05 10:31:03 +0100

    updating filters and tag monitors and internal aws recors

--- module/main.tf
+++ module/main.tf
@@ -15,6 +15,16 @@ resource "aws_route53_record" "record" {
   records = [aws_instance.instance.public_ip]
   ttl = 30
 }
+resource "aws_route53_record" "record-internal" {
+  name    = "${var.tool_name}-internal"
+  type    = "A"
+  zone_id = var.zone_id
+  records = [aws_instance.instance.private_ip]
+  ttl = 30
+}
+
+
+
 resource "aws_iam_role" "role" {
   name = "${var.tool_name}-role"
 
