c33481122de5076d8d0edfa3c81c32c701662197
rajendra <rajendrammarboyana@gmail.com>
2024-04-10 12:08:48 +0100

    spot instances

--- module/main.tf
+++ module/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "instance" {
   instance_market_options {
     market_type = "spot"
     spot_options {
-      instance_interruption_behaviour = "stop"
+      instance_interruption_behavior = "stop"
       spot_instance_type              = "persistent"
     }
   }
